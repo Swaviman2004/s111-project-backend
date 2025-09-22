d5dd343b986587f94ae5a51d111612e6470a5405
Swaviman2004 <162682906+Swaviman2004@users.noreply.github.com>
2025-09-22 23:15:30 +0530

    Update OurstoreBackendApplication.java

--- src/main/java/com/ourstore/ourstorebackend/OurstoreBackendApplication.java
+++ src/main/java/com/ourstore/ourstorebackend/OurstoreBackendApplication.java
@@ -1,13 +1,33 @@
+// package com.ourstore.ourstorebackend;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class OurstoreBackendApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(OurstoreBackendApplication.class, args);
+//     }
+
+// }
+
 package com.ourstore.ourstorebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OurstoreBackendApplication {
+public class OurstoreBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OurstoreBackendApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OurstoreBackendApplication.class);
+    }
 }
